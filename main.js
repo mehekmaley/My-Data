760805fd189966b75ac2df99baefcfaa5e2a8528
Mehek Male <mehekmaley@gmail.com>
2019-06-18 21:09:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ function openBuyForm() {
 function closeBuyForm() {
     document.getElementById("buyForm").style.display = "none";
 }
+
+var inputSeller = document.getElementById("inputSeller");
+
+function addBuy() {
+    var messageSeller = inputSeller.value;
+    
+    var firebaseRef = firebase.database().ref();
+
+    firebaseRef.child('test').set(messageSeller);
+
+    window.alert(messageSeller);
+}
